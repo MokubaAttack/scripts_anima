b749a52badbfd0036c360fe94a389c0cce9aaeb6
mokuba_attack <ninjapink24@gmail.com>
2026-06-07 18:31:47 +0900

    Add files via upload

--- mokuba_colab_anima/source/setup.py
+++ mokuba_colab_anima/source/setup.py
@@ -7,14 +7,13 @@ setup(
 	include_package_data=True,
 	author='mokuba_attack',
 	description='This is a script that I use when I create images by diffusers.',
-	url='https://github.com/MokubaAttack/scripts',
+	url='https://github.com/MokubaAttack/scripts_anima',
 	license='BSD-3-Clause',
 	classifiers=[
 		'License :: OSI Approved :: BSD License',
 		'Programming Language :: Python :: 3.12',
 	],
 	install_requires=[
-		"pyexiv2",
 		"compel>=2.4.0",
 		"torch @ https://download-r2.pytorch.org/whl/cu128/torch-2.11.0%2Bcu128-cp312-cp312-manylinux_2_28_x86_64.whl",
 		"torchvision @ https://download-r2.pytorch.org/whl/cu128/torchvision-0.26.0%2Bcu128-cp312-cp312-manylinux_2_28_x86_64.whl",
